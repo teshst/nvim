16b0d6ded09ec9746cf37764d0d67e3613f45690
Seth <teshst>
2024-03-11 22:41:44 -0600

    made neotree see hidden files, added more plugins

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        --visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".github",
+          ".gitignore",
+          "package-lock.json",
+        },
+        never_show = { ".git" },
+      },
+    },
+  },
+}
